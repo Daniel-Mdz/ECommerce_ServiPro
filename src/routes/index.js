521d6e6fda278dfab96f1d215c08ece801643a1b
Daniel-Mdz <danielrandiss@gmail.com>
2024-08-08 21:55:38 -0500

    Include login/register structure in navigation

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,6 @@ const router = Router()
 router.get('/', (req, res) => res.render('index',{title: 'ServiPro - Los mejores servicios'}))
 router.get('/about', (req, res) => res.render('about', {title: 'ServiPro - Sobre nosotros'}))
 router.get('/contact', (req, res) => res.render('contact', {title: 'ServiPro - Contáctanos'}))
+router.get('/login', (req, res) => res.render('login', {title: 'ServiPro - Login'}))
 
 export default router
